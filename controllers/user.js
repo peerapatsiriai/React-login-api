d080ba4e5cbdd50218bd423c4f0a6c46d8cc64d2
Peerapat <85300665+peerapatsiriai@users.noreply.github.com>
2022-07-17 14:41:33 +0700

    new bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,26 +1,14 @@
 const connection = require('../connection/connect')
 
 // 
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+var bcrypt = require('bcryptjs');
+//const saltRounds = 10;
 
 // Set JWT
 const jwt = require('jsonwebtoken')
 const secret = 'jong'
 
 // ---------------> Login <--------------
-// module.exports.login = (req, res) => {
-//     const {email,password} = req.body
-//     var sql = "SELECT * FROM db_users WHERE email = ? AND password = ?";
-//     connection.query(sql,[email,password], function (err, user) {
-//         if (err) { res.status(400).json({msg:err}); return; }
-//         if (user.length == 0) { res.status(404).json({msg:'Not Found User'}); return; }
-//         const token = jwt.sign({ email: user[0].email }, secret,{expiresIn: '1h'})
-//         res.status(200).json({msg:"Login success",status:"ok", token,})
-//         return;
-//     });
-// }
-
 module.exports.login = (req, res) => {
     const {email,password} = req.body
     var sql = "SELECT * FROM db_users WHERE email = ?";
@@ -42,36 +30,28 @@ module.exports.login = (req, res) => {
 }
 
 // ---------------> Register <--------------
-// module.exports.register = (req, res) => {
-//     const {email,password} = req.body
-//     // encrypt password
-//     var sql = "INSERT INTO db_users (email, password) VALUES (?,?)";
-//         connection.query(sql,[email,password], function (err) {
-//             if (err) {
-//                 res.status(400).json({msg:err})
-//                 return;
-//             } 
-//             res.status(200).json({msg:"OK"})
-//             return;
-//         });
-// }
-
 module.exports.register = (req, res) => {
     const {email,password} = req.body
     // encrypt password
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-        var sql = "INSERT INTO db_users (email, password) VALUES (?,?)";
-        connection.query(sql,[email,hash], function (err) {
-            if (err) {
-                res.status(400).json({msg:err})
+    bcrypt.genSalt(5, function(err, salt) {
+        if(err) { res.status(400).json({msg:err}) }
+        bcrypt.hash(password, salt, function(err, hash) {
+            if(err) { res.status(400).json({msg:err}) }
+            var sql = "INSERT INTO db_users (email, password) VALUES (?,?)";
+            connection.query(sql,[email,hash], function (err) {
+                if (err) {
+                    res.status(400).json({msg:err})
+                    return;
+                } 
+                res.status(200).json({msg:"OK"})
                 return;
-            } 
-            res.status(200).json({msg:"OK"})
-            return;
+            });
         });
     });
 }
 
+
+//
 module.exports.auth = (req, res) => {
     try {
         const token = req.headers.authorization.split(' ')[1]
